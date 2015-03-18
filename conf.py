8d9005499e2288ec1525713582bf3c152ff4f1a7
Rose Coste <rose.coste@rackspace.com>
2015-03-18 10:16:15 -0500

    change copyright year to 2015

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Rackspace Cloud Core Infrastructure Services User Guide'
-copyright = u'2014, Rackspace'
+copyright = u'2015, Rackspace'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
